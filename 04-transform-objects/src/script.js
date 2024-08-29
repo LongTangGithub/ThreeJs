28eec64c386eb421452e471a2ffc494a6cfad1b0
LongTangGithub <tanglh@g.cofc.edu>
2024-08-28 23:10:51 -0400

    Tranforming Objects in ThreeJS Completed

--- 04-transform-objects/src/script.js
+++ 04-transform-objects/src/script.js
@@ -3,16 +3,50 @@ import * as THREE from 'three'
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
 
+
 // Scene
 const scene = new THREE.Scene()
 
 /**
  * Objects
+ * 
+ * 1 Group with 3 Cubes and Rotate the whole group
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+
+const group = new THREE.Group();
+group.position.y = 1;
+group.scale.y = 2;
+group.setRotationFromAxisAngle.y = 1;
+scene.add(group);
+
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: "red" }),
+
+);
+group.add(cube1);
+
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: "green" }),
+
+);
+cube2.position.x = -2;
+group.add(cube2);
+
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: "blue" }),
+
+);
+cube3.position.x = 2;
+group.add(cube3);
+
+
+
+// Axes Helper
+const axesHelper = new THREE.AxesHelper(2)
+scene.add(axesHelper);
 
 /**
  * Sizes
@@ -29,6 +63,11 @@ const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
 scene.add(camera)
 
+// camera.lookAt(new THREE.Vector3(0, - 1, 0))
+// camera.lookAt(mesh.position);
+
+// console.log(mesh.position.distanceTo(camera.position))
+
 /**
  * Renderer
  */
@@ -37,3 +76,4 @@ const renderer = new THREE.WebGLRenderer({
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.render(scene, camera)
+
